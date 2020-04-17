5a8b44327abcdc8fd1b62224403178e26e8f1f6d
Miha Markič <miha@rthand.com>
2020-04-17 17:01:35 +0200

    Adds environmental data

--- sources/CyanometerWeb/Cyanometer.Web/Pages/Location.cshtml.cs
+++ sources/CyanometerWeb/Cyanometer.Web/Pages/Location.cshtml.cs
@@ -11,12 +11,33 @@ namespace Cyanometer.Web.Pages
         public string AirQualitySource { get; private set; }
         public string AirQualityLink { get; private set; }
         public string AirQualityHost => new Uri(AirQualityLink).Host;
+        public string PollutionIcon => "circle";
+        public string PollutionColor => "#F17E19";
+        public string PollutionText => "HIGH";
+        public string LevelsText => "NITROGEN DIOXIDE";
+        public Pollution Pollution { get; private set; }
         public void OnGet(string city, string country)
         {
             City = city.Titleize();
             Country = country.Titleize();
             AirQualitySource = "ARSO Ljubljana Bežigrad";
             AirQualityLink = "https://www.arso.gov.si/";
+            Pollution = new Pollution(92, 13, 4, 6);
+        }
+    }
+
+    public class Pollution
+    {
+        public int Ozone { get; }
+        public int PM10 { get; }
+        public int SO2 { get; }
+        public int NO2 { get; }
+        public Pollution(int ozone, int pM10, int sO2, int nO2)
+        {
+            Ozone = ozone;
+            PM10 = pM10;
+            SO2 = sO2;
+            NO2 = nO2;
         }
     }
 }
